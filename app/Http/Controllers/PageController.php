a7faef55db57b8b454f4928d9c7250a55c855ed8
davidlgabriel <david.g.leal00@gmail.com>
2023-03-12 01:22:35 +0000

    s

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -34,18 +34,16 @@ class PageController extends Controller
         $verificar_atual_aluno= Inscritos::where('nome_quarto',$nome_quarto)->where('situacao_atual',"Alumni")->first();
         dd($verificar_atual_aluno);
         if($quarto->aceite == 0){
-        foreach ($todos as $membro) {
+            if (isset($verificar_atual_aluno)) {
+                $quarto->aceite =1;
+                $quarto->save();
             
-            $membro->aceite =1;
-            $membro->save();
-        }
-           /** colocar 200 */
-        
-   
-          
-                return  back()->with('success', 'Obrigado! A tua inscrição foi registada! Verifica no teu email, a confirmação. Se não encontrares está no spam!');
+                return  back()->with('success', 'Parabéns!!! Conseguiste lugar no melhor village de sempre! Verifica no teu email, a confirmação. Se não encontrares está no spam!');
             }else{
-                return  back()->with('erro', 'OOOPPSS! Algum número de aluno que registou neste formulário já está inscrito! Por favor tente outra vez!');
+                return  back()->with('erro', 'OOOPPSS! Tens alumnis no teu quarto. Vais poder tentar inscrever-te amanhã... Boa sorte :)');
             }
+        }else{
+            return  back()->with('erro', 'OOOPPSS! Este quarto já tinha sido inscrito para ir ao melhor village de sempre!');
+        }
     }
 }
