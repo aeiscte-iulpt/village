32a59ebbbbc3750e94d5b08f214380d3a739bb98
davidlgabriel <david.g.leal00@gmail.com>
2023-03-12 01:50:21 +0000

    b

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -33,22 +33,21 @@ class PageController extends Controller
         $todos= Inscritos::where('nome_quarto',$nome_quarto)->get();
         $verificar_atual_aluno= Inscritos::where('nome_quarto',$nome_quarto)->where('situacao_atual',"Alumni")->first();
         $contagem= Inscritos::where('aceite','!=', 0)->count();
-        dd($contagem);
         if ($contagem <= 86) {
             if($quarto->aceite == 0){
                 if (!isset($verificar_atual_aluno)) {
-                    $quarto->aceite =$contagem + 10;
+                    $quarto->aceite =$contagem + 20;
                     $quarto->save();
-                    return  back()->with('success', 'Parabéns!!! Conseguiste lugar no melhor village de sempre! Verifica no teu email, a confirmação. Se não encontrares está no spam!');
+                    return  back()->with('success', ' Parabéns!!! Conseguiste lugar no melhor village de sempre! Verifica no teu email, a confirmação. Se não encontrares está no spam! ');
                 }else{
-                    return  back()->with('erro', 'OOOPPSS! Tens alumnis no teu quarto. Vais poder tentar inscrever-te amanhã... Boa sorte :)');
+                    return  back()->with('erro', ' OOOPPSS! Tens alumnis no teu quarto. Vais poder tentar inscrever-te amanhã... Boa sorte :) ');
                 }
 
             }else{
-                return  back()->with('erro', 'OOOPPSS! Este quarto já tinha sido inscrito para ir ao melhor village de sempre!');
+                return  back()->with('erro', ' OOOPPSS! Este quarto já tinha sido inscrito para ir ao melhor village de sempre! ');
             }
         }else{
-            return  back()->with('erro', 'OOOPPSSS! Já está esgotado :(');
+            return  back()->with('erro', ' OOOPPSSS! Já está esgotado :( ');
         }
     }
 }
