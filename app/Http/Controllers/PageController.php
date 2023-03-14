d56ecaec9e73eaa436f7e26e424a4dfe4400f0da
davidlgabriel <david.g.leal00@gmail.com>
2023-03-14 23:13:01 +0000

    f

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -32,7 +32,7 @@ class PageController extends Controller
         $nome_quarto=$quarto->nome_quarto;
         $todos= Inscritos::where('nome_quarto',$nome_quarto)->get();
         $verificar_atual_aluno= Inscritos::where('nome_quarto',$nome_quarto)->where('situacao_atual',"Alumni")->first();
-        $contagem= Inscritos::where('aceite','!=', 0)->count();
+        $contagem= Inscritos::where('aceite','!=', 0)->where('aceite','LKE',"ESPERA")->count();
         if ($contagem <= 3) {
             if($quarto->aceite == 0){
                 if (!isset($verificar_atual_aluno)) {
@@ -57,7 +57,7 @@ class PageController extends Controller
             dd($contagem);
             if ($contagem <= 4) {
                 if (!isset($verificar_atual_aluno)) {
-                    $quarto->aceite ="ESPERA";
+                    $quarto->aceite =$contagem + 1000;
                     $quarto->save();
                     $data = [
                         'nome_quarto' => $quarto->nome_quarto,
