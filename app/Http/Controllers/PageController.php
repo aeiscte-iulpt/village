0484695a2c5f2425ad0e5783cf0531dc1fae9ef7
davidlgabriel <david.g.leal00@gmail.com>
2023-03-14 23:15:24 +0000

    h

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -32,7 +32,7 @@ class PageController extends Controller
         $nome_quarto=$quarto->nome_quarto;
         $todos= Inscritos::where('nome_quarto',$nome_quarto)->get();
         $verificar_atual_aluno= Inscritos::where('nome_quarto',$nome_quarto)->where('situacao_atual',"Alumni")->first();
-        $contagem= Inscritos::where('aceite','!=', 0)->where('aceite','LKE',"ESPERA")->count();
+        $contagem= Inscritos::where('aceite','!=', 0)->count();
         if ($contagem <= 3) {
             if($quarto->aceite == 0){
                 if (!isset($verificar_atual_aluno)) {
