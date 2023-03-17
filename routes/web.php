2aba3ef2e60dd54fe9d0d52beda7f64f7b3d556f
davidlgabriel <david.g.leal00@gmail.com>
2023-03-17 05:45:43 +0000

    h

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/inscricao', [App\Http\Controllers\PageController::class, 'inscrever'])->name('inscrever');
+Route::get('/basekkmqjdg', [App\Http\Controllers\PageController::class, 'inscrever'])->name('inscrever');
 Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->name('home');
 Route::post('/inscricao-2023', [App\Http\Controllers\PageController::class, 'inscricao'])->name('inscricao');
 Route::get('/email-just',[App\Http\Controllers\PageController::class, 'email'])->name('email');
